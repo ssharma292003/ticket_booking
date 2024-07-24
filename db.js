bebc761ba38171163d9997c8b222924f56bf5c4c
ssharma292003 <shubhamsharma200329@gmail.com>
2024-07-24 12:46:20 +0530

    added

--- db.js
+++ db.js
@@ -1,57 +1,25 @@
-const { query } = require('express');
+
 
 require('dotenv').config();
 const mysql = require('mysql2');
+const { Client } = require('pg');
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 
-function db() {
-    let conID;
-    var con = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USERNAME, // Replace with your MySQL username (default is 'root')
-        password: process.env.DB_PASSWORD, // Replace with your MySQL password (default is '' or 'root')
-        database: process.env.DB_DBNAME,
-        waitForConnections: true,
+async function db() {
+    const con = new Client({
+        user: 'your_username',
+        host: 'localhost',
+        database: 'your_database',
+        password: 'your_password',
+        port: 5432,
     });
 
-    con.getConnection((err, connection) => {
-        if (err) {
-            console.error('Error connecting to MySQL:', err);
-        } else {
-            conID = connection.threadId;
-            // console.log('Connected as id ' + connection.threadId);
-            connection.query('SHOW TABLES LIKE "BOOKINGS"', (err, tables) => {
-                if (err) {
-                    console.error('Error checking if table exists:', err);
-                } else {
-                    if (tables.length === 0) {
-                        const createTableQuery = `
-                            CREATE TABLE BOOKINGS (
-                                ID INT AUTO_INCREMENT PRIMARY KEY,
-                                MOVIE_NAME VARCHAR(255),
-                                TIME VARCHAR(255),
-                                SHOW_TIME VARCHAR(255),
-                                PEOPLE INT,
-                                PRICE INT,
-                                TOTAL_PRICE INT
-                            )
-                        `;
-                        connection.query(createTableQuery, (err) => {
-                            if (err) {
-                                console.error('Error creating table:', err);
-                            } else {
-                                console.log('Table created successfully');
-                            }
-                        });
-                    } else {
-                        console.log('Table already exists');
-                    }
-                }
-                connection.release(); // Release the connection back to the pool
-            });
-        }
-    });
+    client.connect()
+        .then(() => console.log('Connected to PostgreSQL'))
+        .catch(err => console.error('Connection error', err.stack));
 
-    return con;
+return con;
 }
 
 module.exports = db;
